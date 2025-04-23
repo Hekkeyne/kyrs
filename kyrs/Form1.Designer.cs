daf494f06edf24f3d73cfec41aaf14b1d7d3f29d
Данил Митясов <mrstickmananimation@gmail.com>
2025-04-23 20:49:40 +0700

    Добавьте файлы проекта.

---
+++ kyrs/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿using System.Windows.Forms;
+
+namespace kyrs
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label2 = new Label();
+            button2 = new Button();
+            label3 = new Label();
+            label4 = new Label();
+            dataGridView1 = new DataGridView();
+            button3 = new Button();
+            label1 = new Label();
+            button4 = new Button();
+            openFileDialog1 = new OpenFileDialog();
+            button5 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.AutoSize = true;
+            button1.Location = new Point(312, 68);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 25);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(218, 44);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(90, 25);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(218, 68);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(90, 25);
+            textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BorderStyle = BorderStyle.FixedSingle;
+            label2.Location = new Point(22, 25);
+            label2.Name = "label2";
+            label2.Size = new Size(75, 25);
+            label2.TabIndex = 4;
+            label2.Text = "label2";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(431, 68);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 25);
+            button2.AutoSize = true;
+            button2.TabIndex = 5;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BorderStyle = BorderStyle.FixedSingle;
+            label3.Location = new Point(22, 48);
+            label3.Name = "label3";
+            label3.Size = new Size(75, 25);
+            label3.TabIndex = 6;
+            label3.Text = "label3";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BorderStyle = BorderStyle.FixedSingle;
+            label4.Location = new Point(22, 74);
+            label4.Name = "label4";
+            label4.Size = new Size(75, 25);
+            label4.TabIndex = 7;
+            label4.Text = "label4";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(218, 97);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(1136, 701);
+            dataGridView1.TabIndex = 8;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(629, 68);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 25);
+            button3.AutoSize = true;
+            button3.TabIndex = 9;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BorderStyle = BorderStyle.FixedSingle;
+            label1.Location = new Point(1360, 97);
+            label1.MaximumSize = new Size(788, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(40, 17);
+            label1.TabIndex = 10;
+            label1.Text = "label1";
+            // 
+            // button4
+            // 
+            button4.AutoSize = true;
+            button4.Location = new Point(774, 68);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 25);
+            button4.AutoSize = true;
+            button4.TabIndex = 12;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.RestoreDirectory = true;
+            // 
+            // button5
+            // 
+            button5.AutoSize = true;
+            button5.Location = new Point(312, 44);
+            button5.Name = "button5";
+            button5.Size = new Size(75, 25);
+            button5.TabIndex = 13;
+            button5.Text = "button5";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoScroll = true;
+            AutoSize = true;
+            ClientSize = new Size(1848, 814);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(label1);
+            Controls.Add(button3);
+            Controls.Add(dataGridView1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(button2);
+            Controls.Add(label2);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            WindowState = FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        private Button button1;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label2;
+        private Button button2;
+        private Label label3;
+        private Label label4;
+        private DataGridView dataGridView1;
+        private Button button3;
+        private Label label1;
+        private Button button4;
+        private OpenFileDialog openFileDialog1;
+        private Button button5;
+    }
+}
---
+++ kyrs/Form1.cs
@@ -0,0 +1,180 @@
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace kyrs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();//здесь я добавлял, если мне было лень идти в form1 designer
+            label2.Text = "Ведите количество экспертов и количество товаров";
+            button1.Text = "Создать таблицу"; // быв. Ввод
+            button2.Text = "Ввести случайные данные";
+            label3.Text = "Количество экспертов";
+            label4.Text = "Количество инструментов";
+            button3.Visible = false;
+            button4.Visible = false;
+            label1.Text = "Ответ недоступен, для начала нажмите кнопку 'Создать таблицу'";
+            button5.Text = "Иморт exel файла (wip)";
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int x = rnd.Next(1, 101);
+            int y = rnd.Next(1, 101);
+            textBox1.Text = x.ToString();
+            textBox2.Text = y.ToString();
+            //здесь рандом для 2й кнопки
+        }
+        private void button1_Click(object sender, EventArgs e) // почему-то даже если вылазит ошибка с try то кнопки 3 и 4 всё-равно появляются
+        {
+            try
+            {
+                button3.Text = "Вычислить данные";
+                button3.Visible = true;
+                label1.Text = "Ответ недоступен, нажмите кнопку 'Вычислить данные'";
+                button4.Visible = true;
+                button4.Text = "Зарандомить таблицу";
+                int experts = int.Parse(textBox1.Text);
+                int tovar = int.Parse(textBox2.Text);
+                dataGridView1.Columns.Clear();
+                dataGridView1.Rows.Clear();
+                dataGridView1.Columns.Add("", "/");
+                for (int i = 0; i < tovar; i++)
+                {
+                    dataGridView1.Columns.Add("", $"Товар {1 + i}"); // создание строки с товарами до i-1 столбца
+                }
+                for (int i = 0; i < experts; i++)
+                {
+                    dataGridView1.Rows.Add();
+                    Random f = new Random();
+                    dataGridView1.Rows[i].Cells[0].Value = $"Эксперт {i + 1}";// создаю строчки с экспертами
+                    for (int j = 1; j < tovar + 1; j++)
+                    {
+                        dataGridView1.Rows[i].Cells[j].Value = f.Next(0, 101);//а здесь уже заполняю строчки. Причем тк. 0 столбец у меня занят экспертами, приходится начинать заполнять с j=1 столбца
+                    }
+
+                }
+            }
+            catch// <31> через catch они появляются на секунду
+            {
+                label1.Text = @$"Ошибка button1_Click
+попробуйте больше не пользоваться компьютером";
+                button3.Visible = false;
+                button4.Visible = false;
+            }
+        }
+        private void button3_Click(object sender, EventArgs e) // если нажать кнопку 2, потом 3 без кнопки 1, то будет ошибка на 78 строке, мне лень исправлять
+        {
+            try
+            {
+                double y = 0;
+                int experts = int.Parse(textBox1.Text);
+                int tovar = int.Parse(textBox2.Text);
+                List<double> ans1 = new List<double>();// альтернатива векторам из плюсов
+                for (int i = 1; i < tovar+1 ; i++)// от 1 до tovara тк 0 столбец - текст)))
+                {
+                    for (int j = 0; j < experts; j++)
+                    {
+                        y += double.Parse(dataGridView1.Rows[j].Cells[i].Value.ToString());//суммиру по столбцам
+                    }
+                    ans1.Add(y);
+                    y = 0;
+                }
+                double min = ans1[0];
+                double max = ans1[0];
+                int maxi = 0;
+                int mini = 0;
+                for (int i = 1; i < ans1.Count; i++)// ищу одновременно лучший и худший товар
+                {
+                    if (ans1[i] < min)
+                    {
+                        min = ans1[i];
+                        mini = i;
+                    }
+                    if (ans1[i] > max)
+                    {
+                        max = ans1[i];
+                        maxi = i;
+                    }
+                }
+                label1.Text = @$"1)
+Наихудший товар {mini + 1} с суммарной оценкой {min} 
+Наилучший товар {maxi + 1} с суммарной оценкой {max} 
+";
+                double maxex = double.Parse(dataGridView1.Rows[0].Cells[maxi + 1].Value.ToString());
+                double maxexi = 1;
+                for (int j = 1; j < experts + 1; j++) // поставил j с 1 чтоб было лучше нумеровать экспертов на выводе, в итоге промучался долго с границами (((
+                {
+                    if (maxex < double.Parse(dataGridView1.Rows[j - 1].Cells[maxi + 1].Value.ToString()))
+                    {
+                        maxex = double.Parse(dataGridView1.Rows[j - 1].Cells[maxi + 1].Value.ToString());
+                        maxexi = j;
+                    }
+                }
+                label1.Text += @$"2)
+Наивысший балл {maxex} поставил эксперт {maxexi} товару {maxi + 1}
+3)
+";
+                double x = 0;
+                double[] anss = new double[ans1.Count];
+                for (int i = 0; i < ans1.Count; i++)//заполняю массив с номерами товаров 
+                {
+                    anss[i] = i + 1;
+                }
+                for (int i = 0; i < ans1.Count - 1; i++)//пузырьковая сортировка, только её умею делать
+                {
+                    for (int j = 0; j < ans1.Count - 1 - i; j++)
+                    {
+                        if (ans1[j] < ans1[j + 1])
+                        {
+                            x = ans1[j];
+                            ans1[j] = ans1[j + 1];
+                            ans1[j + 1] = x;
+                            x = anss[j];
+                            anss[j] = anss[j + 1];
+                            anss[j+1] = x;
+                        }//причем сортирую сразу оценку и номер товара
+                    }
+                }
+                for (int i = 0 ; i < ans1.Count; i++)
+                {
+                    label1.Text += $@"Товар {anss[i]}, Общая оценка {ans1[i]}
+";// эта штука мне делает перенос по строка
+                }
+            }
+            catch
+            {
+                label1.Text = @$"Ошибка button3_Click
+Попробуйте заполнить все другими данными или нажать 'Создать таблицу'";
+            }
+        }
+        private void button4_Click(object sender, EventArgs e)//зарандомить таблицу, тупо паста из кнопки 2
+        {
+            int experts = int.Parse(textBox1.Text);
+            int tovar = int.Parse(textBox2.Text);
+            for (int i = 0; i < experts; i++)
+            {
+
+                Random f = new Random();
+                var dd = new DataGridViewRow();
+                dd.CreateCells(dataGridView1);
+                for (int j = 1; j < tovar + 1; j++)
+                {
+                    dataGridView1.Rows[i].Cells[j].Value = f.Next(1, 101);
+                }
+            }
+
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+
+            }
+        }
+        
+    }
+}
